01474fca5089338647f5f1adf3df3773077972ae
Harmony <harmonised7@gmail.com>
2023-02-23 18:57:26 +0000

    First commit

---
+++ practical_1_v3/practical_1_v3.srcs/sources_1/new/main.vhd
@@ -0,0 +1,153 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/10/2023 02:32:27 PM
+-- Design Name: 
+-- Module Name: main - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity main is
+    Port ( clk, reset : in STD_LOGIC;
+           sw : in STD_LOGIC;
+           led : out STD_LOGIC;
+           indicator : out STD_LOGIC);
+end main;
+
+
+architecture Behavioral of main is
+  component kcpsm6 
+    generic(                 hwbuild : std_logic_vector(7 downto 0) := X"00";
+                    interrupt_vector : std_logic_vector(11 downto 0) := X"3FF";
+             scratch_pad_memory_size : integer := 64);
+    port (                   address : out std_logic_vector(11 downto 0);
+                         instruction : in std_logic_vector(17 downto 0);
+                         bram_enable : out std_logic;
+                             in_port : in std_logic_vector(7 downto 0);
+                            out_port : out std_logic_vector(7 downto 0);
+                             port_id : out std_logic_vector(7 downto 0);
+                        write_strobe : out std_logic;
+                      k_write_strobe : out std_logic;
+                         read_strobe : out std_logic;
+                           interrupt : in std_logic;
+                       interrupt_ack : out std_logic;
+                               sleep : in std_logic;
+                               reset : in std_logic;
+                                 clk : in std_logic);
+  end component;
+  
+ component code                          
+    generic(             C_FAMILY : string := "7S"; 
+                C_RAM_SIZE_KWORDS : integer := 1;
+             C_JTAG_LOADER_ENABLE : integer := 0);
+    Port (      address : in std_logic_vector(11 downto 0);
+            instruction : out std_logic_vector(17 downto 0);
+                 enable : in std_logic;
+                    rdl : out std_logic;                    
+                    clk : in std_logic);
+  end component;
+  
+signal            sled : std_logic_vector(7 downto 0);
+signal         address : std_logic_vector(11 downto 0);
+signal     instruction : std_logic_vector(17 downto 0);
+signal     bram_enable : std_logic;
+signal         in_port : std_logic_vector(7 downto 0);
+signal      s_out_port : std_logic_vector(7 downto 0);
+signal         port_id : std_logic_vector(7 downto 0);
+signal    write_strobe : std_logic;
+signal  k_write_strobe : std_logic;
+signal     read_strobe : std_logic;
+signal       interrupt : std_logic;
+signal   interrupt_ack : std_logic;
+signal    kcpsm6_sleep : std_logic;
+signal    kcpsm6_reset : std_logic;
+
+signal    reset_rdl : std_logic;
+
+--
+-- Some additional signals are required if your system also needs to reset KCPSM6. 
+--
+
+signal       cpu_reset : std_logic;
+signal             rdl : std_logic;
+
+--
+-- When interrupt is to be used then the recommended circuit included below requires 
+-- the following signal to represent the request made from your system.
+--
+
+signal     int_request : std_logic;
+
+begin
+
+kcpsm6_reset <= reset_rdl or reset;
+interrupt <= interrupt_ack;
+
+ processor: kcpsm6
+    generic map (                 hwbuild => X"00", 
+                         interrupt_vector => X"3FF",
+                  scratch_pad_memory_size => 64)
+    port map(      address => address,
+               instruction => instruction,
+               bram_enable => bram_enable,
+                   port_id => port_id,
+              write_strobe => write_strobe,
+            k_write_strobe => k_write_strobe,
+                  out_port => sled, --s_out_port,
+               read_strobe => read_strobe,
+                   in_port => in_port,
+                 interrupt => interrupt,
+             interrupt_ack => interrupt_ack,
+                     sleep => '0', --kcpsm6_sleep,
+                     reset => kcpsm6_reset,
+                       clk => clk);
+   program_rom: code                   --Name to match your PSM file
+    generic map(             C_FAMILY => "7S",   --Family 'S6', 'V6' or '7S'
+                    C_RAM_SIZE_KWORDS => 1,      --Program size '1', '2' or '4'
+                 C_JTAG_LOADER_ENABLE => 0)      --Include JTAG Loader when set to '1' 
+    port map(      address => address,      
+               instruction => instruction,
+                    enable => bram_enable,
+                       rdl => reset_rdl,
+                       clk => clk);
+-- latchedLedBit:vi_d_flip_flop port map (clk, sLedIn, sLedOut);
+output_ports : process(clk)
+begin
+    if (clk'event and clk = '1') then
+        if (write_strobe = '1') then
+            if (port_id = "00000001") then
+                sled <= s_out_port;
+            else
+                sled <= sled;
+            end if;
+        end if;
+    end if;
+end process;
+
+indicator <= '1';
+led <= '1'; --sled(0);
+end Behavioral;
---
+++ practical_1_v3/practical_1_v3.srcs/sources_1/new/re_d_flip_flop.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/17/2023 03:02:52 PM
+-- Design Name: 
+-- Module Name: re_d_flip_flop - vi_gate
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity re_d_flip_flop is
+    Port ( Clk : in STD_LOGIC;
+           D : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end re_d_flip_flop;
+
+architecture vi_gate of re_d_flip_flop is
+
+begin
+
+
+end vi_gate;
